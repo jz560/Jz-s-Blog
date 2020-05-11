4cc5ad606d48f0ea814afb63cb604f55e5dd61ae
jz560 <jzz08111@gmail.com>
2020-05-11 14:35:17 +0800

    display articles

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -26,9 +26,14 @@ class Index extends Controller
 
     public function getArticle()
     {
-        $article = Article::get(2);
+        //从前端获取文章id
+        $id=input('param.id');
+        //数据库根据id查询
+        $article = Article::get($id);
+        //获取文章标题和内容
         $title = $article->title;
         $content = $article->content;
+        //传给模板
         $this->assign('title', $title);
         $this->assign('content', $content);
         return view('article');
